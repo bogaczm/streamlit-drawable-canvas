65ae3da45bce29e2aa01cd11fa72f4478ff4d4ee
bogaczm <m.bogacz1997@gmail.com>
2025-12-06 23:05:19 +0100

    setup for pypi

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ def readme() -> str:
 
 
 setuptools.setup(
-    name="streamlit-drawable-canvas",
-    version="0.9.3",
-    author="Fanilo ANDRIANASOLO",
-    author_email="contact@andfanilo.com",
+    name="streamlit-drawable-canvas-bogaczm",
+    version="0.9.4",
+    author="bogaczm",
+    author_email="m.bogacz1997@gmail.com",
     description="A Streamlit custom component for a free drawing canvas using Fabric.js.",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/andfanilo/streamlit-drawable-canvas",
+    url="https://github.com/bogaczm/streamlit-drawable-canvas",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
